9498cf6d6d38c870022004a7c8b3f5b05c0ccdd8
kostyahoriainov <kostiantyn.horiainov@gmail.com>
2018-12-04 14:17:53 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false
+    }
+  }
+
+  componentDidMount() {
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json
+        })
+      }) 
+      
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const {items, isLoaded} = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>
+    } 
+      return (
+        <ul>
+          {items.map(item => {
+            return <li key={item.id}> Name: {item.name}  </li>
+          })}
+        </ul>
+      );
+    
   }
 }
 
